f946735ae701f55ffda664aec74e9809a3462639
enzoeferreira <enzoeferreira@gmail.com>
2024-03-19 18:06:26 -0300

    [ADDED] Atributos

--- Monstro.java
+++ Monstro.java
@@ -1,3 +1,9 @@
 public class Monstro {
-    
+    /**
+     *  ATRIBUTOS
+     */ 
+    private String nome;
+    private int poder;
+    private int quantidadeDeTesouros;
+    private int niveisPerdidos;
 }
