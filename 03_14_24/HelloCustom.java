514f0f140554effc77d06453067b320d17e8ace9
enzoeferreira <enzoeferreira@gmail.com>
2024-03-19 16:34:50 -0300

    [ADDED] HelloCustom.java

---
+++ 03_14_24/HelloCustom.java
@@ -0,0 +1,6 @@
+// Programa Java
+public class HelloCustom {
+    public static void main(String[] args) {
+        System.out.println("Olá "+args[0]+"!");
+    } // Fim do método main
+} // Fim da classe HelloWorld
