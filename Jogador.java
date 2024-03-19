fc7b0c48ffbcc7feb0462ee5a95ef0381313733a
enzoeferreira <enzoeferreira@gmail.com>
2024-03-19 17:45:58 -0300

    [ADDED] Atributos

--- Jogador.java
+++ Jogador.java
@@ -1,3 +1,12 @@
 public class Jogador{
-    
+    // Atributos
+    private String nome;
+    private int nível;
+    private String raça;
+    private String classe;
+    private String itemCabeça;
+    private String itemCorpo;
+    private String itemMaoDireita;
+    private String itemMaoEsquerda;
+    private String itemPe;
 }
