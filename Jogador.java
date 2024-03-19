7895a3fd5ca213188ed4acba252d222907019b8f
enzoeferreira <enzoeferreira@gmail.com>
2024-03-19 17:49:43 -0300

    [ADDED] getters

--- Jogador.java
+++ Jogador.java
@@ -1,7 +1,7 @@
 public class Jogador{
     // Atributos
     private String nome;
-    private int nível;
+    private int nivel;
     private String raça;
     private String classe;
     private String itemCabeça;
@@ -9,4 +9,37 @@ public class Jogador{
     private String itemMaoDireita;
     private String itemMaoEsquerda;
     private String itemPe;
+
+    // getters e setters
+    public String getNome() {
+        return nome;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public String getRaça() {
+        return raça;
+    }
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public String getItemCabeça() {
+        return itemCabeça;
+    }
+
+    public String getItemCorpo() {
+        return itemCorpo;
+    }
+
+    public String getItemMaoDireita() {
+        return itemMaoDireita;
+    }
+
+    public String getItemMaoEsquerda() {
+        return itemMaoEsquerda;
+    }
 }
