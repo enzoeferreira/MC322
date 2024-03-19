a17f5ac2a74300ab7701d5222be200d5153c76db
enzoeferreira <enzoeferreira@gmail.com>
2024-03-19 16:34:38 -0300

    [ADDED] HelloWorld.java

---
+++ 03_14_24/HelloWorld.java
@@ -0,0 +1,6 @@
+// Programa Java
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+    } // Fim do método main
+} // Fim da classe HelloWorld
