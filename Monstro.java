d45eca31412572828f492d85fecd193520bdc0c8
enzoeferreira <enzoeferreira@gmail.com>
2024-03-19 18:03:49 -0300

    [ADDED] getters e setters

--- Monstro.java
+++ Monstro.java
@@ -6,4 +6,43 @@ public class Monstro{
     private int bonusPoder;
     private int valor;
     private String tipo;
+
+    
+    /**
+     * MÉTODOS
+     */
+    
+    // getters
+    public String getNome() {
+        return nome;
+    }
+
+    public int getBonusPoder() {
+        return bonusPoder;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    // setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setBonusPoder(int bonusPoder) {
+        this.bonusPoder = bonusPoder;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
