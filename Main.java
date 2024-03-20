86edcd294d73a8f6c3c09dc42b05318a187c9318
enzoeferreira <enzoeferreira@gmail.com>
2024-03-19 22:23:38 -0300

    [ADDED] Instancia teste

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import Munchkin.*;
+
 public class Main {
     public static void main(String[] args) {
         // Método main
+        Jogador J1 = new Jogador("Alberto", "Humano", "Mago");
+        System.out.println(J1);
     }
 }
